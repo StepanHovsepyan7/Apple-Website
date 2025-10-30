87a9e199feb6f9dbdca68deff03d7fa8005c36aa
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-10-31 00:58:04 +0400

    navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
+    <main>
+        <NavBar/>
+    </main>
   )
 }
 
